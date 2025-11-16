ae7f841c093b2f60b07d22e191288bdfe62d1cca
huey-nibiru <yousefmacer@gmail.com>
2025-11-15 20:37:16 -0500

    update about

--- new_earth/src/pages/about/about.jsx
+++ new_earth/src/pages/about/about.jsx
@@ -6,15 +6,92 @@ const About = () => {
       <h1 className="about-header">About</h1>
       <div className="about-details">
         <p>
-          There will always be an emphasis on putting <strong>THE BIBLE FIRST</strong>, but a huge problem for me and many others who walked away from faith was not having a proper church or group of people that truly broke down what it meant to be Christian in a modern world. Much of what I disliked about Christianity was actually <strong>CARNAL CHRISTIANITY</strong>. Eventually if you want truth you'll find it; there are plenty of resources online that can help you understand exactly how to grow in faith and/or operate in the world as someone with faith.
+          There will always be an emphasis on putting{" "}
+          <strong>THE BIBLE FIRST</strong>, but a huge problem for me and many
+          others who walked away from faith was not having a proper church or
+          group of people that truly broke down what it meant to be Christian in
+          a modern world. Much of what I disliked about Christianity was
+          actually <strong>CARNAL CHRISTIANITY</strong>. Eventually if you want
+          truth you'll find it; there are plenty of resources online that can
+          help you understand exactly how to grow in faith and/or operate in the
+          world as someone with faith.
         </p>
         <p>
-          Everything will always be downstream from the Bible, but the world is often incompatible with the true nature of Christ's teachings. Theres only a small amount of Christians who truly follow the Bible, and even fewer who have an applied perspective on how a Christ centered society could operate. After realizing how much I still do not understand, I wanted a hub where both experienced Christians and new Christians could organize and explore different aspects of modern life through a True Christian lens. The main focus is having other people with a Christian perspective give insight into both spiritual and material concerns. Whether you're Orthodox, Catholic, Protestant, or even a NonChristian who is for the first time interested in learning the True path of our Lord Jesus Christ, you're welcome here.
+          Everything will always be downstream from the Bible, but the world is
+          often incompatible with the true nature of Christ's teachings. Theres
+          only a small amount of Christians who truly follow the Bible, and even
+          fewer who have an applied perspective on how a Christ centered society
+          could operate. After realizing how much I still do not understand, I
+          wanted a hub where both experienced Christians and new Christians
+          could organize and explore different aspects of modern life through a
+          True Christian lens. The main focus is having other people with a
+          Christian perspective give insight into both spiritual and material
+          concerns. Whether you're Orthodox, Catholic, Protestant, or even a
+          NonChristian who is for the first time interested in learning the True
+          path of our Lord Jesus Christ, you're welcome here.
         </p>
+        <p>
+          New Earth is a centralized hub for exploring Christian thought across
+          12 key life domains. The navigation hub featuring a central gold cross
+          icon surrounded by 12 navigation points represents a different aspect
+          of life. A click on the central cross will route you to the Bible.
+        </p>
+
+        <p>
+          Each section has its own theme but remains interconnected with the
+          other sections. Dial in to your expertise and share insights,
+          experiences, and strategies. Ask questions that intrigue you and
+          receive answers from others.{" "}
+        </p>
+        <p>
+          <strong>12 Core Sections</strong>
+        </p>
+        <ol className="about-details-list" style={{ textAlign: "left" }}>
+          <li>
+            <strong>Education & Knowledge</strong> - Educational content (math,
+            science, history, random facts, etc). All knowledge leads back to
+            God.{" "}
+          </li>
+          <li>
+            <strong>Family & Community Life</strong> - Family-focused resources{" "}
+          </li>
+          <li>
+            <strong>Governance & Justice</strong> - Governance and legal
+            information{" "}
+          </li>
+          <li>
+            <strong>Economy & Work</strong> - Economic and employment resources{" "}
+          </li>
+          <li>
+            <strong>Health & Well-being</strong> - Share diet, gym routines,
+            mental health practices{" "}
+          </li>
+          <li>
+            <strong>Agriculture & Food Security</strong> - Agricultural
+            information{" "}
+          </li>
+          <li>
+            <strong>Technology & Innovation</strong> - Tech news, engineering,
+            collaboration{" "}
+          </li>
+          <li>
+            <strong>Art & Music</strong> - Creative content and artistic works{" "}
+          </li>
+          <li>
+            <strong>Service & Charity</strong> - Post available services and
+            charitable work{" "}
+          </li>
+          <li>
+            <strong>Environment & Creation Care</strong> - Proper care of your
+            surroundings surroundings{" "}
+          </li>
+          <li>
+            <strong>Defense & Safety</strong> - Self Desfense and training
+          </li>
+        </ol>
       </div>
     </div>
   );
 };
 
 export default About;
-
